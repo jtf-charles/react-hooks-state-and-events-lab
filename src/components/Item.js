d691db47f453e0d31f21cee372ad6f39915589de
unknown <jtf-charles>
2025-07-01 21:53:21 -0500

    Lab completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React , { useState }  from "react";
+
+
 
 function Item({ name, category }) {
+  const [cartState, setCart]=useState(false);
+
+  function cartModeHandle(){
+    setCart(!cartState);
+  }
+
+  const appCart = cartState? "in-cart" : ""
   return (
-    <li className="">
+    <li className={appCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={cartModeHandle}>{cartState? "Remove From Cart": "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory]=useState("All");
+ let filteredItems;
+
+  function selectHandle(event){
+    setSelectedCategory(event.target.value)
+  }
+
+  if (selectedCategory==="All"){
+    filteredItems=items
+  }else {
+    filteredItems=items.filter(item => item.category===selectedCategory)
+  }
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={selectHandle}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
